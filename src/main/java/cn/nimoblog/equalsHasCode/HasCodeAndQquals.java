6cf442a42c58a8a3e5cb5d88d82e72d17e16e8bc
chufan <907701820@qq.com>
2021-09-30 14:04:34 +0800

    1

--- src/main/java/cn/nimoblog/equalsHasCode/HasCodeAndQquals.java
+++ src/main/java/cn/nimoblog/equalsHasCode/HasCodeAndQquals.java
@@ -1,2 +1,37 @@
-package cn.nimoblog.equalsHasCode;public class HasCodeAndQquals {
+package cn.nimoblog.equalsHasCode;
+
+import lombok.Data;
+
+import java.util.Objects;
+
+/**
+ * 重写 equals 和 hasCode 方法
+ */
+@Data
+public class HasCodeAndQquals {
+
+    private String name;
+
+    private int age;
+
+    private String card;
+
+    private String address;
+
+    private String phone;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        HasCodeAndQquals that = (HasCodeAndQquals) o;
+        return Objects.equals(getName(), that.getName()) &&
+                Objects.equals(getAddress(), that.getAddress());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), getName(), getAddress());
+    }
 }
