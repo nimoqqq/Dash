0fe9a7f4a27b0c3a7923b73de084e351a1de860a
chufan <907701820@qq.com>
2021-11-30 19:28:58 +0800

    基本 io

---
+++ sysio/src/main/java/com/chuf/sys/io/FileInputStreamTest.java
@@ -0,0 +1,73 @@
+package com.chuf.sys.io;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Objects;
+
+public class FileInputStreamTest {
+    private static final Logger log = LoggerFactory.getLogger(FileInputStreamTest.class);
+    private static final String path = "./sysio/xxoo.txt";
+
+    public static void main(String[] args) {
+        readFile1024();
+    }
+
+    /**
+     * 一次读取一个字节
+     */
+    public static void readFile() {
+        File file = new File(path);
+        FileInputStream fis = null;
+        try {
+            fis = new FileInputStream(file);
+            while (!Objects.equals(-1, fis.read())) {
+                log.info("输出:{}", fis.read());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            //关闭资源
+            try {
+                fis.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * try-with-resource
+     * 关闭资源
+     */
+    public static void readFileTWE() {
+        File file = new File(path);
+        try(FileInputStream fis = new FileInputStream(file)) {
+            while (!Objects.equals(-1, fis.read())) {
+                log.info("输出:{}", fis.read());
+            }
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 一次读取 1024 个字节
+     */
+    public static void readFile1024() {
+        File file = new File(path);
+        byte[] data = new byte[1024];
+        int readlen = 0;
+        try(FileInputStream fis = new FileInputStream(file)) {
+            while ((readlen = fis.read(data)) != -1) {
+                log.info("输出:{}", new String(data, 0, readlen));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ sysio/src/main/java/com/chuf/sys/io/FileTest.java
@@ -0,0 +1,40 @@
+package com.chuf.sys.io;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FileTest {
+    private static final Logger log = LoggerFactory.getLogger(FileTest.class);
+    private static final String path = "./sysio/xxoo.txt";
+
+    public static void main(String[] args) {
+        testFile();
+    }
+
+    public static void testFile() {
+        //只是在java 中创建了一个对象
+        File file = new File(path);
+        try {
+            //在磁盘创建该文件
+            boolean newFile = file.createNewFile();
+            log.info("文件创建成功{}", newFile);
+            log.info("文件名称:{}", file.getName());
+            log.info("文件绝对路径:{}", file.getAbsolutePath());
+            log.info("文件父级目录:{}", file.getParent());
+            log.info("文件大小:{}", file.length());
+            log.info("文件是否存在:{}", file.exists());
+            log.info("是不是一个文件:{}", file.isFile());
+            log.info("是不是同一个目录:{}", file.isDirectory());
+//            log.info("删除文件:{}", file.delete());
+        } catch (IOException e) {
+            e.printStackTrace();
+            log.error(String.valueOf(e));
+        }
+    }
+
+
+}
