073d75c44122664e741c8701e10eed2624c115bf
chufan <907701820@qq.com>
2021-12-14 10:32:32 +0800

    gson

--- jdk17/src/main/java/com/chuf/javase/GsonTest.java
+++ jdk17/src/main/java/com/chuf/javase/GsonTest.java
@@ -1,2 +1,15 @@
-package com.chuf.javase;public class GsonTest {
+package com.chuf.javase;
+
+import com.google.gson.Gson;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GsonTest {
+    public static void main(String[] args) {
+        String result = "{\"skuId\":\"413451097\",\"source\":\"1\",\"pageNo\":1,\"pageSize\":10,\"blurShopId\":\"\",\"blurShopName\":\"\",\"blurCityName\":\"\"}";
+        Map data = new Gson().fromJson(result, HashMap.class);
+        System.out.println(data);
+        System.out.println(data.get("skuId"));
+    }
 }
